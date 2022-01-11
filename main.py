873353ca9a7e8d9888e5880ab79067f76b3d912e
q-chihiro <ura@qoonest.co.jp>
2022-01-11 09:21:18 +0900

    add something

--- main.py
+++ main.py
@@ -7,14 +7,74 @@ This can be used to demonstrate concurrent send and recieve over OSC
 import argparse
 import random
 import time
-import math
 import threading
 import numpy as np
 import cv2
+import tkinter as tk
+from PIL import Image, ImageTk, ImageOps
 
 from pythonosc import udp_client
 from pythonosc import dispatcher
 from pythonosc import osc_server
+
+
+class TmView(tk.Tk):
+    def __init__(self, analyzer, server):
+        self.analyzer = analyzer
+        self.server = server
+
+        super().__init__()
+        self.title("TouchMatrix Viewer")
+
+        self.view_frame = tk.Frame(self, pady=10, padx=10, relief=tk.GROOVE, bd=2)
+        self.control_frame = tk.Frame(self, pady=10, padx=10, relief=tk.GROOVE, bd=2)
+        self.view_frame.grid(row=0, column=0, columnspan=2)
+        self.control_frame.grid(row=1, column=0)
+
+        self.canvas = tk.Canvas(self.view_frame, height=300, width=600)
+        self.canvas.pack(expand=True, fill=tk.BOTH)
+
+        self.server_start_button = tk.Button(self.control_frame, text="Start Server", command=self.server.start_server, width=40)
+        self.analyze_start_button = tk.Button(self.control_frame, text="Start Analyze", command=self.analyzer.start, width=40)
+        self.cal_lower_button = tk.Button(self.control_frame, text="Lower", command=self.analyzer.calibration_lower, width=40)
+        self.cal_upper_button = tk.Button(self.control_frame, text="Upper", command=self.analyzer.calibration_upper, width=40)
+        self.read_button = tk.Button(self.control_frame, text="Read Image", command=self.__update_image, width=40)
+        self.server_start_button.pack()
+        self.analyze_start_button.pack()
+        self.cal_lower_button.pack()
+        self.cal_upper_button.pack()
+        self.read_button.pack()
+
+    def __update_image(self):
+        if self.analyzer.disp_img is None:
+            self.view_frame.after(100, self.__update_image)
+            return
+
+        tmp = (self.analyzer.disp_img * 255).astype(np.uint8)
+        self.disp_image(tmp)
+
+        self.view_frame.after(30, self.__update_image)
+
+    def disp_image(self, img):
+        # NumPyのndarrayからPillowのImageへ変換
+        pil_image = Image.fromarray(img)
+
+        # キャンバスのサイズを取得
+        canvas_width = self.canvas.winfo_width()
+        canvas_height = self.canvas.winfo_height()
+
+        # 画像のアスペクト比（縦横比）を崩さずに指定したサイズ（キャンバスのサイズ）全体に画像をリサイズする
+        pil_image = ImageOps.pad(pil_image, (canvas_width, canvas_height))
+
+        # PIL.ImageからPhotoImageへ変換する
+        self.photo_image = ImageTk.PhotoImage(image=pil_image)
+
+        # 画像の描画
+        self.canvas.create_image(
+            canvas_width / 2,  # 画像表示位置(Canvasの中心)
+            canvas_height / 2,
+            image=self.photo_image  # 表示画像データ
+        )
 
 
 class TmFrame:
@@ -44,56 +104,38 @@ class Analyzer(threading.Thread):
         self.cal_state = 0
         self.cal_max = None
         self.cal_min = None
+        self.range = None
 
         self.led_insert_pos = self.__insert_led()
 
-        self.grad_img = cv2.imread('resources/grad.png', cv2.IMREAD_GRAYSCALE)
-        self.plot_img = np.zeros((330, 660), np.uint16)
-
-    def __paste(self, img, imgback, x, y, angle, scale):
-        # x and y are the distance from the center of the background image
-
-        r = img.shape[0]
-        c = img.shape[1]
-        rb = imgback.shape[0]
-        cb = imgback.shape[1]
-        hrb = round(rb / 2)
-        hcb = round(cb / 2)
-        hr = round(r / 2)
-        hc = round(c / 2)
-
-        # Copy the forward image and move to the center of the background image
-        imgrot = np.zeros((rb, cb), np.uint16)
-        imgrot[hrb - hr:hrb + hr, hcb - hc:hcb + hc] = img[:hr * 2, :hc * 2]
-
-        # Rotation and scaling
-        M = cv2.getRotationMatrix2D((hcb, hrb), angle, scale)
-        imgrot = cv2.warpAffine(imgrot, M, (cb, rb))
-        # Translation
-        M = np.float32([[1, 0, x], [0, 1, y]])
-        imgrot = cv2.warpAffine(imgrot, M, (cb, rb))
-
-        # Makeing mask
-        imggray = cv2.cvtColor(imgrot, cv2.COLOR_BGR2GRAY)
-        ret, mask = cv2.threshold(imggray, 10, 255, cv2.THRESH_BINARY)
-        mask_inv = cv2.bitwise_not(mask)
-
-        # Now black-out the area of the forward image in the background image
-        img1_bg = cv2.bitwise_and(imgback, imgback, mask=mask_inv)
-
-        # Take only region of the forward image.
-        img2_fg = cv2.bitwise_and(imgrot, imgrot, mask=mask)
-
-        # Paste the forward image on the background image
-        imgpaste = cv2.add(img1_bg, img2_fg)
-
-        return imgpaste
+        self.grad_size = 200
+        self.plot_size = (330, 660)
+        self.over_scan = 100
+        self.grad_img = cv2.imread('resources/grad.png', cv2.IMREAD_GRAYSCALE) / 255
+        self.grad_img = cv2.resize(self.grad_img, [self.grad_size, self.grad_size])
+        self.plot_img = None
+        self.disp_img = None
 
     def __insert_led(self):
-            pos = []
-            for i in range(11):
-                pos.extend(range((i % 2) + (i * 11), (i + 1) * 11 + (i % 2), 1))
-            return pos
+        pos = []
+        for i in range(11):
+            pos.extend(range((i % 2) + (i * 11), (i + 1) * 11 + (i % 2), 1))
+        return pos
+
+    def calibration_lower(self):
+        # self.cal_min = self.__tm_frame.n_array
+        self.cal_min = np.random.randint(0, 60000, 121, np.uint16)
+        print(self.cal_min)
+        self.cal_state = self.CAL_MIN
+
+    def calibration_upper(self):
+        # self.cal_max = self.__tm_frame.n_array
+        self.cal_max = np.random.randint(0, 60000, 121, np.uint16)
+        print(self.cal_max)
+        self.range = self.cal_max - self.cal_min
+        # self.range[self.range < 0] = 0
+        print(self.range)
+        self.cal_state = self.CAL_MAX
 
     def __calibration(self, data):
         if self.cal_state < self.CAL_MIN:
@@ -118,91 +160,103 @@ class Analyzer(threading.Thread):
             self.__loop()
             time.sleep(0.03)
 
+    def __clear_plot(self):
+        extra_px = self.over_scan * 2
+        self.plot_img = np.zeros((self.plot_size[0] + extra_px, self.plot_size[1] + extra_px))
+
+    def __plot(self, sensor_data):
+        self.__clear_plot()
+        sens_height, sens_width = sensor_data.shape[:2]
+        xp = int(self.plot_size[1] / (sens_width - 1))   # step x
+        yp = int(self.plot_size[0] / (sens_height - 1))  # step y
+        grad_h = int(self.grad_size / 2)
+
+        for hi in range(sens_height):
+            for wi in range(sens_width):
+                y = (hi * yp) + self.over_scan
+                x = (wi * xp) + self.over_scan
+                tmp = (self.grad_img * sensor_data[hi, wi])
+                self.plot_img[y - grad_h:y + grad_h, x - grad_h:x + grad_h] += tmp
+
+        self.plot_img[self.plot_img > 1.0] = 1.0
+        self.disp_img = self.plot_img
+
     def __loop(self):
-        data = self.__tm_frame.n_array
+        # data = self.__tm_frame.n_array
+        data = np.random.randint(0, 60000, 121, np.uint16)
+
         if data is None:
             return
 
-        self.__calibration(data)
-        if self.cal_min is not None and self.cal_max is not None:
-            data[data <= self.cal_min] = self.cal_min[data <= self.cal_min]
-            offset = data - self.cal_min
-
-            offset[offset >= self.range] = self.range[offset >= self.range]
-
-            calc = (offset / self.range)
-            calc[calc >= 1.0] = 1.0
-            # calc = (calc ** 0.7 )* 65535.0
-            # calc = (np.sin(np.pi * (calc - 0.1)) + 1) / 2 * 65535
-            calc = (calc * 3) * 65535.0
-            calc[calc >= 65535.0] = 65535.0
-            n_array = calc.astype(np.uint16)
-        else:
+        # self.__calibration(data)
+        if self.cal_min is None or self.cal_max is None:
             return
 
-        n_array = np.insert(n_array, self.led_insert_pos, 0)
-        n_array = np.reshape(n_array, (11, 22))
+        # replace out-of-range values (lower)
+        data[data <= self.cal_min] = self.cal_min[data <= self.cal_min]
+        offset = data - self.cal_min
+
+        # replace out-of-range values (upper)
+        offset[offset >= self.range] = self.range[offset >= self.range]
+
+        # normalize
+        calc = (offset / self.range)
+        calc[calc >= 1.0] = 1.0
+
+        # gamma correction
+        """calc = (calc ** 0.7 )* 65535.0"""
+        """calc = (np.sin(np.pi * (calc - 0.1)) + 1) / 2 * 65535"""
+        """calc = (calc * 3)
+        calc[calc >= 1.0] = 1.0"""
+
+        calc = np.insert(calc, self.led_insert_pos, 0)
+        calc = np.reshape(calc, (11, 22))
+
+        self.__plot(calc)
+
+        # img3 = cv2.resize(n_array, (int(22 * 50), int(11 * 50)), interpolation=cv2.INTER_NEAREST)
+        # cv2.imshow('ir', self.plot_img.astype(np.uint16))
+        # cv2.waitKey(1)
 
 
+class OSCServer:
+    def __init__(self, tm_frame, ip="127.0.0.1", port=7000):
+        self.ip = ip
+        self.port = port
+        self.server = None
+        self.buffer = tm_frame
 
-        img3 = cv2.resize(n_array, (int(22 * 50), int(11 * 50)), interpolation=cv2.INTER_NEAREST)
-        cv2.imshow('ir', img3)
-        cv2.waitKey(1)
+        # listen to addresses and print changes in values
+        self.dispatcher = dispatcher.Dispatcher()
+        self.dispatcher.map("/sensor_value", self.set_buffer)
 
+    def set_addr(self, ip, port):
+        self.ip = ip
+        self.port = port
 
-def print_sensor_value(unused_addr, tm_frame_, value1, value2):
-    tm_frame_[0].add_pixel(value1, value2)
-    if value1 == 120:
-        tm_frame_[0].finalize()
+    def set_buffer(self, value1, value2):
+        self.buffer.add_pixel(value1, value2)
+        if value1 == 120:
+            self.buffer.finalize()
 
+    def start_server(self):
+        print("Starting Server")
+        self.server = osc_server.ThreadingOSCUDPServer(
+            (self.ip, self.port), self.dispatcher)
+        print("Serving on {}".format(self.server.server_address))
+        thread = threading.Thread(target=self.server.serve_forever)
+        thread.start()
 
-def start_server(ip, port):
-
-    print("Starting Server")
-    server = osc_server.ThreadingOSCUDPServer(
-        (ip, port), dispatcher)
-    print("Serving on {}".format(server.server_address))
-    thread = threading.Thread(target=server.serve_forever)
-    thread.start()
-
-
-def start_client(ip, port):
-    print("Starting Client")
-    client = udp_client.SimpleUDPClient(ip, port)
-    # print("Sending on {}".format(client.))
-    thread = threading.Thread(target=random_values(client))
-    thread.start()
-
-
-# send random values between 0-1 to the three addresses
-def random_values(client):        
-    while True:
-        for x in range(10):
-            client.send_message("/1/fader2", random.random())
-            client.send_message("/1/fader1", random.random())
-            client.send_message("/1/xy1", [random.random(), random.random()])
-            time.sleep(.5)
+    def stop(self):
+        self.server.stop()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--serverip", default="192.168.137.1", help="The ip to listen on")
-    parser.add_argument("--serverport", type=int, default=7000, help="The port the OSC Server is listening on")
-    parser.add_argument("--clientip", default="127.0.0.1", help="The ip of the OSC server")
-    parser.add_argument("--clientport", type=int, default=5005, help="The port the OSC Client is listening on")
-    args = parser.parse_args()
 
-    # touch matrix frame instance initialize
-    tm_frame = TmFrame()
+    t_frame = TmFrame()
 
-    # listen to addresses and print changes in values
-    dispatcher = dispatcher.Dispatcher()
-    dispatcher.map("/sensor_value", print_sensor_value, tm_frame)
+    t_server = OSCServer(t_frame)
+    t_analyzer = Analyzer(t_frame)
 
-    start_server(args.serverip, args.serverport)
-    # start_client(args.clientip, args.clientport)
-
-    analyzer = Analyzer(tm_frame)
-    analyzer.start()
-
-
+    t_view = TmView(t_analyzer, t_server)
+    t_view.mainloop()
