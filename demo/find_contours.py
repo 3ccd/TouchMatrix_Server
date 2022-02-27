9ca7a405ebb7e435a43419ff974939dd5c41c8f6
q-chihiro <ura@qoonest.co.jp>
2022-02-27 19:01:33 +0900

    デモを追加

--- demo/find_contours.py
+++ demo/find_contours.py
@@ -1,27 +1,54 @@
-import random
 import time
 from abc import ABC
 from demo.visualizer import DemoContents, Visualizer
 import cv2
 import numpy as np
 
+import requests
+
 
 class FindContours(DemoContents, ABC):
 
     def __init__(self, visualizer):
         super().__init__(visualizer)
-        self.name = "Find Contours"
+        self.name = "Car Controller"
+        self.touch_rec = [0, 0]
+        self.status = 0
 
     def draw(self):
         self.clear_frame()
+        if not self.visualizer.is_touch:
+            return
 
-        obj_img = self.visualizer.get_object_image(True)
-        contours, hierarchy = cv2.findContours(
-            obj_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        contours_img = np.zeros_like(obj_img)
-        cv2.drawContours(contours_img, contours, -1, color=(0, 0, 255), thickness=2)
+        y = int(self.visualizer.frame_size[0] / 2)
+        if self.status == 1:
+            cv2.arrowedLine(self.frame, (100, y), (500, y), (255, 255, 255), thickness=20,
+                            tipLength=0.5)
+        elif self.status == 2:
+            cv2.arrowedLine(self.frame, (500, y), (100, y), (255, 255, 255), thickness=20,
+                            tipLength=0.5)
 
-        self.frame = cv2.resize(contours_img, dsize=(self.visualizer.frame_size[1], self.visualizer.frame_size[0]))
+        control = self.touch_rec[0] - self.visualizer.touch_pos[0]
+        if -20 < control < 20 and self.status != 0:
+            print("stop")
+            requests.get("http://192.168.32.2/F")
+            self.status = 0
+        elif control > 20 and self.status != 1:
+            print("forward")
+            requests.get("http://192.168.32.2/R")
+            self.status = 1
+        elif control < -20 and self.status != 2:
+            requests.get("http://192.168.32.2/G")
+            print("back")
+            self.status = 2
+
+    def touch_down(self):
+        self.touch_rec = self.visualizer.touch_pos
+
+    def touch_up(self):
+        print("stop")
+        requests.get("http://192.168.32.2/F")
+        self.status = 0
 
 
 if __name__ == "__main__":
